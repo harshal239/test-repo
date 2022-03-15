38b8d6d7347610064e50e5177017bb9cf01e090d
harshal239 <harshalwalunj49@gmail.com>
2022-03-15 21:59:36 +0530

    Made changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="text-3xl underline h-screen flex justify-center items-center">
-      This is a clean React TailwindCSS Template
+      This is a clean React TailwindCSS Template(Made Some Changes here...)
     </div>
   );
 }
